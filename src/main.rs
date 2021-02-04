c2afeebf06de3df2880aa3fde91cc7d30b4c0b7f
pbellchambers <pbellchambers@users.noreply.github.com>
2021-02-04 19:35:15 +0000

    fix: create logfile in correct location

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ extern crate simplelog;
 
 use simplelog::*;
 use std::fs::File;
+use std::{env, process};
 
 fn main() {
     initialise_log();
@@ -17,12 +18,18 @@ fn main() {
 }
 
 fn initialise_log() {
+    let mut file_location = env::current_exe().unwrap_or_else(|err| {
+        error!("Problem getting current executable location: {}", err);
+        process::exit(1);
+    });
+    file_location.pop();
+    file_location.push("rustybox.log");
     CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
         WriteLogger::new(
             LevelFilter::Info,
             Config::default(),
-            File::create("rustybox.log").unwrap(),
+            File::create(file_location.as_path()).unwrap(),
         ),
     ])
     .unwrap();
