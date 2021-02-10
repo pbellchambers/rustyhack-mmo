9ce616f52d4ac1e896e090f379f9b4bc8f665b44
pbellchambers <pbellchambers@users.noreply.github.com>
2021-02-10 00:15:08 +0000

    feat: some initial client/server code

--- rustyhack_client/src/main.rs
+++ rustyhack_client/src/main.rs
@@ -1,3 +1,37 @@
+use laminar::{Packet, Socket};
+use std::time::Instant;
+
 fn main() {
-    //do nothing yet
+    const SERVER_ADDR: &'static str = "127.0.0.1:50201";
+
+    let mut socket = Socket::bind("127.0.0.1:50202").unwrap();
+    let packet_sender = socket.get_packet_sender();
+
+    let unreliable = Packet::reliable_unordered(
+        SERVER_ADDR.parse().unwrap(),
+        String::from("Test1").into_bytes(),
+    );
+    let unreliable1 = Packet::unreliable(
+        SERVER_ADDR.parse().unwrap(),
+        String::from("Test2").into_bytes(),
+    );
+    let unreliable2 = Packet::unreliable(
+        SERVER_ADDR.parse().unwrap(),
+        String::from("Test3").into_bytes(),
+    );
+    let unreliable3 = Packet::unreliable(
+        SERVER_ADDR.parse().unwrap(),
+        String::from("Test4").into_bytes(),
+    );
+    let unreliable4 = Packet::unreliable(
+        SERVER_ADDR.parse().unwrap(),
+        String::from("Test5").into_bytes(),
+    );
+
+    let test = packet_sender.send(unreliable);
+    packet_sender.send(unreliable1);
+    packet_sender.send(unreliable2);
+    packet_sender.send(unreliable3);
+    packet_sender.send(unreliable4);
+    socket.manual_poll(Instant::now());
 }
--- rustyhack_server/src/main.rs
+++ rustyhack_server/src/main.rs
@@ -8,12 +8,15 @@ mod viewport;
 extern crate log;
 extern crate simplelog;
 
+use laminar::{Socket, SocketEvent};
 use simplelog::*;
 use std::fs::File;
-use std::{env, process};
+use std::net::SocketAddr;
+use std::{env, process, thread};
 
 fn main() {
     initialise_log();
+    run_server();
     engine::run(41, 15, 15);
     info!("Program terminated.");
 }
@@ -24,7 +27,7 @@ fn initialise_log() {
         process::exit(1);
     });
     file_location.pop();
-    file_location.push("rustybox.log");
+    file_location.push("rustyhack_server.log");
     CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
         WriteLogger::new(
@@ -34,4 +37,38 @@ fn initialise_log() {
         ),
     ])
     .unwrap();
+}
+
+fn run_server() {
+    const SERVER_ADDR: &'static str = "127.0.0.1:50201";
+    let mut socket = Socket::bind(SERVER_ADDR).unwrap();
+
+    let event_receiver = socket.get_event_receiver();
+    // Starts the socket, which will start a poll mechanism to receive and send messages.
+    let _thread = thread::spawn(move || socket.start_polling());
+
+    loop {
+        // Waits until a socket event occurs
+        let result = event_receiver.recv();
+
+        match result {
+            Ok(socket_event) => match socket_event {
+                SocketEvent::Packet(packet) => {
+                    let endpoint: SocketAddr = packet.addr();
+                    let received_data: &[u8] = packet.payload();
+                    info!("Packet received from {}: {:?}", endpoint, String::from_utf8_lossy(received_data));
+                }
+                SocketEvent::Connect(connect_event) => {
+                    info!("Client connected from: {}", connect_event)
+                }
+                SocketEvent::Timeout(timeout_event) => {
+                    info!("Client timed out {}", timeout_event)
+                }
+                _ => {}
+            },
+            Err(e) => {
+                error!("Something went wrong when receiving, error: {:?}", e);
+            }
+        }
+    }
 }
